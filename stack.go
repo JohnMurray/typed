c5b0822b19a06bbfb35d9d6083d986546a43c8ae
John Murray <me@johnmurray.io>
2021-12-27 15:08:21 -0500

    add comments to Stack[T]

--- stack.go
+++ stack.go
@@ -7,10 +7,13 @@ import "fmt"
 // `make` as a regular slice.
 type Stack[T any] []T
 
+// Push puts an element onto the top of the stack.
 func (s *Stack[T]) Push(element T) {
 	*s = append(*s, element)
 }
 
+// Pop returns the top element from the stack. If the stack is empty an
+// error will be returned. Else the error will always be nil
 func (s *Stack[T]) Pop() (T, error) {
 	if len(*s) == 0 {
 		var nope T
